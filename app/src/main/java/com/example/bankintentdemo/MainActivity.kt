a825e436766f132f64b4203a5b078b8e6f84cd9a
Jeongbin <haj721426@gmail.com>
2026-05-26 03:24:20 +0900

    #1 [ADD] 라이브러리 및 키보드 대응 설정 추가

--- app/src/main/java/com/example/bankintentdemo/MainActivity.kt
+++ app/src/main/java/com/example/bankintentdemo/MainActivity.kt
@@ -3,45 +3,25 @@ package com.example.bankintentdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import com.example.bankintentdemo.ui.MainScreen
 import com.example.bankintentdemo.ui.theme.BankIntentDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            BankIntentDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = Color(0xFFF4F6F9) // 배경색
+            ) {
+                BankIntentDemoTheme(darkTheme = false) { // darkTheme = false
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BankIntentDemoTheme {
-        Greeting("Android")
     }
 }
